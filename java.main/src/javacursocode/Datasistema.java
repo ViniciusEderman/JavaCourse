b9348828f2d61b25d07e782386de54889c20178b
Vinicius Ederman Magalhães Coelho <vinicius-edermanmc@hotmail.com>
2022-02-04 13:37:43 -0300

    Create Datasistema.java

---
+++ java.main/src/javacursocode/Datasistema.java
@@ -0,0 +1,12 @@
+package javacursocode;
+
+import java.util.Date;
+
+public class Datasistema {
+    public static void main(String[] args) {
+      Date relogio = new Date();
+      System.out.println("A hora do sistema é: ");
+      System.out.print(relogio.toString());
+    }
+    
+}
