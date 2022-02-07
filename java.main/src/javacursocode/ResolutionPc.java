2a2b4b2b0b0dc4b0d87c0cc8335c155273f33ba9
Vinicius Ederman Magalhães Coelho <vinicius-edermanmc@hotmail.com>
2022-02-07 12:28:51 -0300

    Create ResolutionPc.java

---
+++ java.main/src/javacursocode/ResolutionPc.java
@@ -0,0 +1,14 @@
+package javacursocode;
+
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
+public class ResolutionPc {
+    public static void main(String[] args) {
+        Toolkit tk = Toolkit.getDefaultToolkit();
+        Dimension d = tk.getScreenSize();
+        System.out.println("Resolução - largura = " + d.width);    
+        System.out.println("Screen - altura = " + d.height);  
+    }    
+}
+
