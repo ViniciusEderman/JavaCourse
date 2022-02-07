ae382e313185025fec68c2781aee66a8a6b18834
Vinicius Ederman Magalhães Coelho <vinicius-edermanmc@hotmail.com>
2022-02-07 14:01:45 -0300

    Update EntradaDeDados.java

--- java.main/src/estudo/EntradaDeDados.java
+++ java.main/src/estudo/EntradaDeDados.java
@@ -4,9 +4,11 @@ import java.util.Scanner;
 public class EntradaDeDados {
     public static void main(String[] args) {
         Scanner objeto = new Scanner(System.in);
+        System.out.println("Digite o nome do aluno: ");
         String nome = objeto.nextLine();
+        System.out.print("Digite a nota do aluno: ");
         float nota = objeto.nextFloat();
-        System.out.format("A nota de %s é de %.1f \n", nome, nota);
+        System.out.format("A nota de %s é de %.2f \n", nome, nota);
         objeto.close();
 }
 
