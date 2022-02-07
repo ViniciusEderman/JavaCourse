8937d97b3a3e06d4627844f0abc1c2176f7359b5
Vinicius Ederman Magalhães Coelho <vinicius-edermanmc@hotmail.com>
2022-02-07 12:56:18 -0300

    Create Types.java

---
+++ java.main/src/estudo/Types.java
@@ -0,0 +1,23 @@
+package estudo;
+
+// conteúdo para lembrar conceitos básicos da linguagem 
+public class Types {
+
+    // TIPOS PRIMITIVOS:
+    int idadeteste = 10;
+    int idade = (int) 3;  // typecast /
+    // Integer idade2 = new  Integer(3); // novo objeto - Wrapper Class
+
+    float sal = 1280.125f;
+    float sal2 = (float) 1122.15;
+    // Float sal3 = new Float(1825.19);
+
+    char letra = 'V';
+    char letra2 = (char) 'G';
+    // Character letra3 = new Character('G');
+
+    boolean casado = true;
+    boolean casado2 = (boolean) false;
+    // Boolean casado3 = new Boolean(false);
+
+}
