c4ffaeed89f573e759ac52b1391808b6479c9458
Vinicius Ederman Magalhães Coelho <vinicius-edermanmc@hotmail.com>
2022-02-08 11:43:17 -0300

    Update Datasistema.java

--- java.main/src/javacursocode/Datasistema.java
+++ java.main/src/javacursocode/Datasistema.java
@@ -2,7 +2,7 @@ package javacursocode;
 
 import java.util.Date;
 
-public class Datasistema {
+public class DataSistema {
     public static void main(String[] args) {
       Date relogio = new Date();
       System.out.println("A hora do sistema é: ");
