c37876e3562547f1b4aeea2a3a442363bcd1368c
Vinicius Ederman Magalhães Coelho <vinicius-edermanmc@hotmail.com>
2022-02-10 10:14:50 -0300

    jogo do adivinha em java

---
+++ java.main/src/javacursocode/JogoAdivinha.java
@@ -0,0 +1,28 @@
+package javacursocode;
+import java.util.Scanner;
+import java.util.Random;
+
+public class JogoAdivinha {
+    public static void main(String[] args) {
+    Scanner armazenarNum = new Scanner(System.in);
+   
+ Random geradornum = new Random();
+    while(true){
+            int random = geradornum.nextInt(5);
+            System.out.println("Digite um numero entre 0 a 5: ");
+            Integer numero = armazenarNum.nextInt();
+            if (numero == random) {
+                System.out.println("Você acertou, o número era:" + random);
+                break;
+            }
+        else {
+          System.out.println("Você errou, o número era:" + random);
+
+        }
+     
+    }
+     armazenarNum.close();
+    
+
+    }
+}
