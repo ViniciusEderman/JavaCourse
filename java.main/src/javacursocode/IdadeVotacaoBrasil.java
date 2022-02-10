76eecd93484ca8c2b23b04079f0d087dbe426be2
Vinicius Ederman Magalhães Coelho <vinicius-edermanmc@hotmail.com>
2022-02-10 12:17:42 -0300

    Create IdadeVotacaoBrasil.java

---
+++ java.main/src/javacursocode/IdadeVotacaoBrasil.java
@@ -0,0 +1,22 @@
+package javacursocode;
+import java.util.Scanner;
+
+public class IdadeVotacaoBrasil {
+    public static void main(String[]args) {
+    Scanner idade = new Scanner(System.in);
+    System.out.println("Digite a idade para descobrir as opções de votação. Digite aqui:" );
+    Integer idadeescolhida = idade.nextInt();
+
+        if ((idadeescolhida >= 0) &&  (idadeescolhida < 16)) {
+            System.out.println("Não vota");
+        } if ((idadeescolhida >=16 ) && (idadeescolhida <18)) {
+            System.out.println("Voto opcional");
+        } if ((idadeescolhida >=18) && (idadeescolhida <=70)) {
+            System.out.println("Voto obrigatório");
+        } if (idadeescolhida > 70) {
+            System.out.println("Voto opcional");
+        }
+idade.close();  
+    }
+}
+// esse código pode ser encurtado, modificando o voto de 70(opcional) e colocando no primeiro if 
