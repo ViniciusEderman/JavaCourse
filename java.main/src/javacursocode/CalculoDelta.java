9da4c68beca1f4a9b574822353a94315cee603ae
Vinicius Ederman Magalhães Coelho <vinicius-edermanmc@hotmail.com>
2022-02-10 11:34:54 -0300

    Create CalculoDelta.java

---
+++ java.main/src/javacursocode/CalculoDelta.java
@@ -0,0 +1,21 @@
+package javacursocode;
+import java.util.Scanner;
+
+public class CalculoDelta {
+    public static void main (String[] args){
+    Scanner objeto = new Scanner(System.in);
+    System.out.println("Escolha o valor de A :");
+    float a = objeto.nextFloat();
+    System.out.println("Escolha o valor de B :");
+    float b =  objeto.nextFloat();
+    System.out.println("Escolha o valor de C :");
+    float c = objeto.nextFloat();
+    double delta = (b * b) -4 *(a*c);
+    System.out.println(delta);
+
+
+    objeto.close();
+    
+    }    
+}
+// variavel delta = (b.b) - 4.(a.c) //
