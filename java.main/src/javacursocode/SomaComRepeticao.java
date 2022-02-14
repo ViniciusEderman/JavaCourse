62b82c54b4da92f08ec60d743768bc7e6557e0a6
Vinicius Ederman Magalhães Coelho <vinicius-edermanmc@hotmail.com>
2022-02-14 10:11:59 -0300

    NEW EXERCISE;

---
+++ java.main/src/javacursocode/SomaComRepeticao.java
@@ -0,0 +1,23 @@
+package javacursocode;
+import java.util.Scanner;
+
+public class SomaComRepeticao {
+    public static void main (String[] args){
+    int soma, x;
+    soma = 0;
+    String responda = new String();
+    Scanner entrada = new Scanner(System.in);
+    
+    do {
+        System.out.println("Digite um número: ");
+        x = entrada.nextInt();
+        soma += x;
+        System.out.println("Deseja continuar? Digite: (S/N) ");
+        responda = entrada.next();
+    } while (responda.equals("S"));
+        System.out.println("A soma de todos os valores é:" + soma);
+
+    entrada.close();    
+    }   
+}
+// FUNCIONANDO 100% // 
