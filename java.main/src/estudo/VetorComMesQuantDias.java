9ad54eda0cc313ff9803ae4212ea1acfc8b17a1b
Vinicius Ederman Magalhães Coelho <vinicius-edermanmc@hotmail.com>
2022-02-15 12:29:46 -0300

    VETOR APRENDIZADO

---
+++ java.main/src/estudo/VetorComMesQuantDias.java
@@ -0,0 +1,13 @@
+package estudo;
+
+public class VetorComMesQuantDias {
+    public static void main(String[] args) {
+        String mes[] = {"jan", "fev", "mar", "abr", "mai", "jun", "jul", "ago", "set", "out", "nov", "dez"};
+        int dias [] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        
+        for(int cont = 0; cont< mes.length; cont++) {
+            System.out.println("O mês de " + mes[cont] + " tem " + 
+            dias[cont] + " dias ao total.");
+        }
+    }
+}
