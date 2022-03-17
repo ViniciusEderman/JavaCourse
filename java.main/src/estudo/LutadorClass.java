3abbeb15265f7ef2b9f68f24913405cdebcaa054
Vinicius Ederman Magalhães Coelho <vinicius-edermanmc@hotmail.com>
2022-03-17 12:01:17 -0300

    push problem

--- java.main/src/estudo/LutadorClass.java
+++ java.main/src/estudo/LutadorClass.java
@@ -1,6 +1,6 @@
 package estudo;
 
-public class LutadorMainClass {
+public class LutadorClass {
     public static void main(String[] args) {
         LutadorFirstClass l = new LutadorFirstClass("Carlos Alberto", "brasileiro", 27, 1.85, 75.9, 17, 3, 1);
         l.apresentar();
