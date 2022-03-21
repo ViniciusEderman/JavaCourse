1b9d4af20593374213d7f6d5fbd2af168c469d6a
Vinicius Ederman Magalhães Coelho <87389323+ViniciusEderman@users.noreply.github.com>
2022-03-21 12:35:09 -0300

    Update LutadorClass.java

--- java.main/src/estudo/LutadorClass.java
+++ java.main/src/estudo/LutadorClass.java
@@ -7,11 +7,11 @@ public class LutadorClass {
         
         LutadorFirstClass l_2[] = new LutadorFirstClass[3];
 
-        l_2[0] = new LutadorFirstClass("Lucas Lu", "brasileiro", 35, 1.90, 80.0, 21, 6, 4);
+        l_2[0] = new LutadorFirstClass("Lucas Lu", "brasileiro", 35, 1.90, 79.0, 21, 6, 4);
 
         l_2[1] = new LutadorFirstClass("Isaac Chiento", "france", 29, 1.69, 65.4, 7, 0, 1);
 
-        l_2[2] = new LutadorFirstClass("Vinicius Carlo", "france", 22, 1.75, 80.0, 13, 0, 0);
+        l_2[2] = new LutadorFirstClass("Vinicius Carlo", "france", 22, 1.75, 82.0, 13, 0, 0);
         
        ClassFight UEC0 = new ClassFight();
        UEC0.marcarLuta(l_2[0], l_2[2]);
