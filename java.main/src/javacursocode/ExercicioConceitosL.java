b50a17f9db7bcac3f6542b315ee433e9c700e415
Vinicius Ederman Magalhães Coelho <vinicius-edermanmc@hotmail.com>
2022-03-22 10:54:37 -0300

    Iniciando novo exercício

---
+++ java.main/src/javacursocode/ExercicioConceitosL.java
@@ -0,0 +1,58 @@
+package javacursocode;
+
+public class ExercicioConceitosL {
+    public String livro;
+    public String autor;
+    public int totPags;
+    public int pagAtual;
+    public boolean aberto; 
+    public String leitor;
+
+    public String getLivro() {
+        return livro;
+    }
+    public void setLivro(String l) {
+        this.livro = l;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+    public void setAutor(String a) {
+        this.autor = a;
+    }
+
+    public int getTotPaginas() {
+        return totPags;
+    }
+    public void setTotPaginas(int tp) {
+        this.totPags = tp;
+    }
+
+    public int getPaginaAtual() {
+        return pagAtual;
+    }
+    public void setPaginaAtual(int pa) {
+        this.pagAtual = pa;
+    }
+
+    public boolean getAberto() {
+        return aberto;
+    }
+    public void setAberto(boolean sn) {
+        this.aberto = sn;
+    }
+
+    public String getLeitor() {
+        return leitor;
+    }
+    public void setLeitor(String leit) {
+        this.leitor = leit;
+    }
+
+    public void detalhes() {
+
+    }
+
+
+}
---
+++ java.main/src/javacursocode/ExercicioConceitosP.java
@@ -0,0 +1,34 @@
+package javacursocode;
+
+public class ExercicioConceitosP {
+    public String nome;
+    public int idade;
+    public char sexo;
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String n) {
+        this.nome = n;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+    public void setNome(int id) {
+        this.idade = id;
+    }
+
+    public char getSexo() {
+        return sexo;
+    }
+    public void setSexo(char s) {
+        this.sexo = s;
+    }
+
+    public void fazerAniv() {
+
+    }
+
+
+}
