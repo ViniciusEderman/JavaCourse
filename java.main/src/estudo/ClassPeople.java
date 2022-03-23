1f30aa78045479da4b48ff6e4c5f068767a87c4f
Vinicius Ederman Magalhães Coelho <vinicius-edermanmc@hotmail.com>
2022-03-23 10:39:23 -0300

    Studying heritage

---
+++ java.main/src/estudo/ClassPeople.java
@@ -0,0 +1,39 @@
+package estudo;
+
+public class ClassPeople {
+// Atributos    
+
+    private String name; 
+    private String sex;     
+    private int yearsOld;
+
+//  Metódos 
+
+    public void doHappy() {
+
+    }
+
+    public int getYearsOld() {
+        return yearsOld;
+    }
+
+    public void setYearsOld(int yearsOld) {
+        this.yearsOld = yearsOld;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
