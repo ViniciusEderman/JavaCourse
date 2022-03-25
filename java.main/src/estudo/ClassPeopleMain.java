75f880819b7e4361538f6788e3b80d1217016990
Vinicius Ederman Magalhães Coelho <vinicius-edermanmc@hotmail.com>
2022-03-25 11:42:02 -0300

    Update ClassPeopleMain.java

--- java.main/src/estudo/ClassPeopleMain.java
+++ java.main/src/estudo/ClassPeopleMain.java
@@ -7,11 +7,13 @@ public class ClassPeopleMain {
     ClassPeopleStudent s2 = new ClassPeopleStudent();
     ClassPeopleTeacher t1 = new ClassPeopleTeacher();
     ClassPeopleEmployee e1 = new ClassPeopleEmployee();
+    ClassPeopleVisitant v1 = new ClassPeopleVisitant();;
 
     p1.setName("Vinicius Roberto Nunes");
     s2.setName("Maria Eduarda");
     t1.setName("Clarice Mariciano");
     e1.setName("Enjhuano Clarico");
+    v1.setName("Emanuel Santos");
 
     p1.setCPF(0);
     s2.setCPF(0);
@@ -22,15 +24,18 @@ public class ClassPeopleMain {
     s2.setSex("F");
     t1.setSex("F");
     e1.setSex("M");
+    v1.setSex("M");
 
     p1.setYearsOld(25);
     s2.setYearsOld(19);
     t1.setYearsOld(29);
     e1.setYearsOld(33);
+    v1.setYearsOld(27);
     
     System.out.println(p1.toString());
     System.out.println(s2.toString());
     System.out.println(t1.toString());
     System.out.println(e1.toString());
+    System.out.println(v1.toString());
   }  
 }
