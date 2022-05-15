051f568d50cf2172f2b34714d806df3de2f9203d
Vinicius Ederman Magalhães Coelho <vinicius-edermanmc@hotmail.com>
2022-05-15 11:03:48 -0300

    Update DIOQuestionOne.java

--- java.main/src/Monitoria/DioExercise/DIOQuestionOne.java
+++ java.main/src/Monitoria/DioExercise/DIOQuestionOne.java
@@ -4,10 +4,12 @@ import java.util.Scanner;
 public class DIOQuestionOne {
     public static void main(String[] args) {
       Scanner scan = new Scanner(System.in);
+      System.out.println(scan); // apenas para parar de reclamar do scan
       String input = scan.nextLine();  
       
       String[] inputSplit = input.split("");
       double media = Double.parseDouble(inputSplit[0]) / Double.parseDouble(inputSplit[1]);
+      
     
       System.out.printf("%.2f", media);
     }
