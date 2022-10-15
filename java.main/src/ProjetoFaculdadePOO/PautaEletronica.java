96093f2b299183a248e2f4d9860dbac4fad3da5c
Vinicius Ederman Magalhães Coelho <vinicius-edermanmc@hotmail.com>
2022-10-15 20:16:06 -0300

    Criando a class pauta

---
+++ java.main/src/ProjetoFaculdadePOO/PautaEletronica.java
@@ -0,0 +1,18 @@
+package ProjetoFaculdadePOO;
+
+public class PautaEletronica {
+    static void Menu() {
+
+        // chamar o metodo construtor == inserir aluno
+
+        // chamar o metodo get == search study
+
+        // chamar o metodo set == inserir nota
+
+        // chamar o metodo set == excluir aluno
+
+        // chamar um get == apresentacao
+
+        // informação da disciplina == getter (big gett)
+    }
+}
