c36527dcfd39c55544dc035555107ac04544ad5c
Vinicius Ederman Magalhães Coelho <vinicius-edermanmc@hotmail.com>
2022-10-15 20:15:51 -0300

    Criando a classe main

---
+++ java.main/src/ProjetoFaculdadePOO/Main.java
@@ -0,0 +1,10 @@
+package ProjetoFaculdadePOO;
+
+public class Main {
+    public static void main (String [] args) {
+    
+        Aluno teste = new Aluno("Vinicius", 10, 10, 7, "aprovado");
+
+        teste.setInserirAluno();
+    }
+}
