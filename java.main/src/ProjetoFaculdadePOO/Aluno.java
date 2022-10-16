4a4a775fdd6703b364c0da94d6a727a47ae23ff0
Vinicius Ederman Magalhães Coelho <vinicius-edermanmc@hotmail.com>
2022-10-16 14:39:07 -0300

    Update

--- java.main/src/ProjetoFaculdadePOO/Aluno.java
+++ java.main/src/ProjetoFaculdadePOO/Aluno.java
@@ -7,24 +7,26 @@ class Aluno {
     private float nota1, nota2, nota3, media;
     
     //construtor:
-    public Aluno(String nome, float nota1, float nota2, float nota3, String situacao) {
+    public Aluno(String nome, float nota1, float nota2, float nota3) {
         // situacao == aprovado, reprovado e de recuperacao
         this.nome = nome;
         this.nota1 = nota1;
         this.nota2 = nota2;
         this.nota3 = nota3;
-        this.situacao = situacao;
     }
 
     // sobrecarga do construtor aluno
-    public Aluno(String disciplina) {
-
-        this.disciplina = disciplina;
+    public Aluno(String nome, float nota1, float nota2, float nota3, String disciplina) {
+        this.nome = nome;
+        this.nota1 = nota1;
+        this.nota2 = nota2;
+        this.nota3 = nota3;
+        this.disciplina = "Matemática";
     }
 
     //metodo responsável por apresentar o Aluno
     public void apresentarAluno() {
-        System.out.println("O aluno: " + this.nome + " participante da disciplina: " + this.disciplina);
+        System.out.println("O aluno: " + this.nome + " participante da disciplina: " + this.disciplina + "o aluno está atualmente: " + situacao);
     }
 
     //metódo para inserir alunos no array
@@ -55,24 +57,24 @@ class Aluno {
         if(this.nota1 >= this.nota2 && this.nota2 > this.nota3 || this.nota2 > this.nota1) {
             media = (this.nota1 + this.nota2)/2;
         }
-        if(this.nota2 > this.nota1 && this.nota3 > this.nota1) {
+        else if(this.nota2 > this.nota1 && this.nota3 > this.nota1) {
             media = (this.nota2 + this.nota3)/2;
         }
-        if(this.nota1 > this.nota2 && this.nota3 > this.nota2) {
+        else if(this.nota1 > this.nota2 && this.nota3 > this.nota2) {
             media = (this.nota1 + this.nota3)/2;
         }
 
         //bloco de verificacao de media:
         if(media >= 6) {
-            this.situacao = "aprovado";
+            situacao = "aprovado";
             System.out.println(situacao);
         }
-        if(media >= 4 && nota3 == 0) {
-            this.situacao = "recuperação";
+        else if(media >= 4 && nota3 == 0) {
+            situacao = "recuperação";
             System.out.println(situacao);
         }
-        if(media < 6) {
-            this.situacao = "reprovado";
+        else if(media < 6) {
+            situacao = "reprovado";
             System.out.println(situacao);
         }
     }
--- java.main/src/ProjetoFaculdadePOO/Main.java
+++ java.main/src/ProjetoFaculdadePOO/Main.java
@@ -3,8 +3,6 @@ package ProjetoFaculdadePOO;
 public class Main {
     public static void main (String [] args) {
     
-        Aluno teste = new Aluno("Vinicius", 10, 10, 7, "aprovado");
-
-        teste.setInserirAluno();
+        
     }
 }
